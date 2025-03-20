795b466512f71e0f9c32f3a0158646ab1c29d454
Flyingfool95 <jhernehult@gmail.com>
2025-03-20 15:22:33 +0100

    Adding timeout on auth-check

--- src/features/auth/components/RouteGuard.tsx
+++ src/features/auth/components/RouteGuard.tsx
@@ -8,19 +8,31 @@ export default function RouteGuard({ isProtected }: { isProtected: boolean }) {
     const { user, setUser, isAuthChecked, setIsAuthChecked } = useAuthStore((state) => state);
 
     const handleCheckAuth = async () => {
-        const result = await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/v1/auth/auth-check`, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            credentials: "include",
-        });
+        const controller = new AbortController();
+        const timeoutId = setTimeout(() => controller.abort(), 5000);
 
-        const res = await result.json();
-        setIsAuthChecked(true);
-        if (!res.success) return setUser(null);
-        setUser(res.data);
+        try {
+            const result = await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/v1/auth/auth-check`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                credentials: "include",
+                signal: controller.signal,
+            });
+            
+            const res = await result.json();
+            setIsAuthChecked(true);
+            if (!res.success) return setUser(null);
+            clearTimeout(timeoutId);
+            setUser(res.data);
+        } catch (error) {
+            clearTimeout(timeoutId);
+            setIsAuthChecked(true);
+            setUser(null);
+        }
     };
+
     useEffect(() => {
         handleCheckAuth();
     }, []);
