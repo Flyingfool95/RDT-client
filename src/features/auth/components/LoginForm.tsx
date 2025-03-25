4e75ba6189dc86bdb72fa3fd61817c479e348cd1
Flyingfool95 <jhernehult@gmail.com>
2025-03-25 12:10:57 +0100

    Setting up styling system and styling LoginForm and RegisterForm

--- src/features/auth/components/LoginForm.tsx
+++ src/features/auth/components/LoginForm.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import "../styles/LoginForm.css"
+import "../styles/LoginForm.css";
 import useAuth from "../hooks/useAuth";
+import { Link } from "react-router-dom";
 
 export default function LoginForm() {
     const { loginUser } = useAuth();
@@ -16,19 +17,32 @@ export default function LoginForm() {
     return (
         <>
             <form onSubmit={handleSubmit}>
-                <label htmlFor="email">Email</label>
-                <input type="email" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                <label htmlFor="email">
+                    Email
+                    <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </label>
 
-                <label htmlFor="password">Password</label>
-                <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
+                <label htmlFor="password">
+                    Password
+                    <input
+                        type="password"
+                        name="password"
+                        id="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </label>
 
                 <input type="submit" value="Login" />
+                <span>
+                    Not a user yet? <Link to="/register">Register here!</Link>
+                </span>
             </form>
 
             {loginUser.isPending && <h1>Loging in...</h1>}
--- src/features/auth/components/RegisterForm.tsx
+++ src/features/auth/components/RegisterForm.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
+import "../styles/RegisterForm.css";
 import useAuth from "../hooks/useAuth";
+import { Link } from "react-router-dom";
 
 export default function RegisterForm() {
     const { registerUser } = useAuth();
@@ -17,27 +19,42 @@ export default function RegisterForm() {
     return (
         <>
             <form onSubmit={handleSubmit}>
-                <label htmlFor="email">Email</label>
-                <input type="email" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                <label htmlFor="email">
+                    Email
+                    <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </label>
 
-                <label htmlFor="password">Password</label>
-                <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                <label htmlFor="confirm-password">Password</label>
-                <input
-                    type="password"
-                    name="confirm-password"
-                    id="confirm-password"
-                    value={confirmPassword}
-                    onChange={(e) => setConfirmPassword(e.target.value)}
-                />
+                <label htmlFor="password">
+                    Password
+                    <input
+                        type="password"
+                        name="password"
+                        id="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </label>
+                <label htmlFor="confirm-password">
+                    Confirm Password
+                    <input
+                        type="password"
+                        name="confirm-password"
+                        id="confirm-password"
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                    />
+                </label>
 
                 <input type="submit" value="Register" />
+                <span>
+                    Allready a user? <Link to="/login">Login here!</Link>
+                </span>
             </form>
         </>
     );
