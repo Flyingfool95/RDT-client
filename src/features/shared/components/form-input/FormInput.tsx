7a2a17007ff7026560847123fe3bbc17b4b67024
FlyingFool95 <jhernehult@gmail.com>
2025-07-07 14:10:00 +0200

    Removing console.log

--- src/features/shared/components/form-input/FormInput.tsx
+++ src/features/shared/components/form-input/FormInput.tsx
@@ -3,7 +3,6 @@ import { FormInputProps } from "./types";
 
 export default function FormInput({ label, name, type, data, setData, placeholder, required }: FormInputProps) {
     const id = `input-${name}`;
-    console.log(data);
     return (
         <div className={`form-input ${data.isError ? "input-error" : ""}`}>
             <label htmlFor={id}>{label}</label>
