0fdd42379683bc250e5445b23aa7b33eb8723e8a
FlyingFool95 <jhernehult@gmail.com>
2025-08-06 22:03:31 +0200

    adding comments for further steps

--- src/features/auth/hooks/useLogin.ts
+++ src/features/auth/hooks/useLogin.ts
@@ -1,4 +1,6 @@
 export default function useLogin() {
+    /* Make login call to backend using ReactQuery */
+    /* Then make routeguard when user is in state */
     function loginUser() {
         console.log("loging in...");
     }
