bfa7d5bdfec9d33aeda8ef061d612d226bcb739b
FlyingFool95 <jhernehult@gmail.com>
2025-08-07 11:16:48 +0200

    Adding comments to planout the auth auto login and logout flow.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { RouterProvider } from "react-router";
 import { router } from "./routes/router";
 
 function App() {
-    /* Check if has token and authorize the token ot get valid user back (react query) */
+    /* Check if has token and authorize the token ot get valid user back to stroe in cache */
     return <RouterProvider router={router} />;
 }
 
