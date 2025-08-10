24b70d2a1e9ad88526e24bb71b44b6a623a3a63f
FlyingFool95 <jhernehult@gmail.com>
2025-08-10 22:34:48 +0200

    Moving the success and error handeling into the Login component - Removing try catch in api as TastackQuery takes care of it

--- src/features/auth/api/login.api..ts
+++ src/features/auth/api/login.api..ts
@@ -1,17 +1,15 @@
 export default async function login(formData: any) {
-    try {
-        let result = await fetch(import.meta.env.VITE_RDT_SERVER_URL + `/api/v1/auth/login`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(formData),
-        });
+    let result: any = await fetch(import.meta.env.VITE_RDT_SERVER_URL + `/api/v1/auth/login`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+    });
 
-        result = await result.json();
+    result = await result.json();
 
-        console.log(result);
-    } catch (error) {
-        console.log(error);
-    }
+    if (!result.success) throw new Error(result.errors[0].message);
+
+    return result.data;
 }
--- src/features/auth/pages/Login.tsx
+++ src/features/auth/pages/Login.tsx
@@ -3,60 +3,55 @@ import useLogin from "../hooks/useLogin";
 
 export default function Login() {
     const [formData, setFormData] = useState({ email: "", password: "" });
+    const [error, setError] = useState("");
 
     const { loginUser } = useLogin();
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
-        loginUser.mutate(formData);
-    };
+        loginUser.mutate(formData, {
+            onSuccess: (result) => {
+                console.log(result);
+            },
 
+            onError: (err) => {
+                console.log(err);
+                setError(err.message);
+            },
+        });
+    };
     return (
         <div>
             <h1>Login</h1>
-            <Form handleSubmit={handleSubmit} formData={formData} setFormData={setFormData} />
-        </div>
-    );
-}
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Email
+                    <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        value={formData.email}
+                        placeholder="my@email.com"
+                        onChange={(e) =>
+                            setFormData((prev) => ({ ...prev, email: e.target.value.toLocaleLowerCase() }))
+                        }
+                    />
+                </label>
+                <label>
+                    Password
+                    <input
+                        type="password"
+                        name="password"
+                        id="password"
+                        value={formData.password}
+                        onChange={(e) => setFormData((prev) => ({ ...prev, password: e.target.value }))}
+                    />
+                </label>
+                <input type="submit" value="Login" />
 
-function Form({
-    handleSubmit,
-    formData,
-    setFormData,
-}: {
-    handleSubmit: (e: React.FormEvent) => void;
-    formData: { email: string; password: string };
-    setFormData: React.Dispatch<
-        React.SetStateAction<{
-            email: string;
-            password: string;
-        }>
-    >;
-}) {
-    return (
-        <form onSubmit={handleSubmit}>
-            <label>
-                Email
-                <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    value={formData.email}
-                    placeholder="my@email.com"
-                    onChange={(e) => setFormData((prev) => ({ ...prev, email: e.target.value }))}
-                />
-            </label>
-            <label>
-                Password
-                <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    value={formData.password}
-                    onChange={(e) => setFormData((prev) => ({ ...prev, password: e.target.value }))}
-                />
-            </label>
-            <input type="submit" value="Login" />
-        </form>
+                {/* Make FormError component */}
+                <p>{error}</p>
+            </form>
+        </div>
     );
 }
