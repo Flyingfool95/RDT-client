9923a22b03e32951bd67eb1cd6abcb7f62abf503
FlyingFool95 <jhernehult@gmail.com>
2025-08-10 22:35:34 +0200

    Removing trycatch from authCheck.api.ts as TanstackQuery takes  care of it

--- src/features/auth/api/authCheck.api.ts
+++ src/features/auth/api/authCheck.api.ts
@@ -1,11 +1,7 @@
 export default async function authCheck() {
-    try {
-        let result = await fetch(import.meta.env.VITE_RDT_SERVER_URL + `/api/v1/auth/auth-check`, {
-            credentials: "include",
-        });
+    let result = await fetch(import.meta.env.VITE_RDT_SERVER_URL + `/api/v1/auth/auth-check`, {
+        credentials: "include",
+    });
 
-        return await result.json();
-    } catch (error) {
-        console.log(error);
-    }
+    return await result.json();
 }
