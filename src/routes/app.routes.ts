1a39df4aca2738aff9c8628301ce6f6a2bc712bb
FlyingFool95 <jhernehult@gmail.com>
2025-08-12 09:33:27 +0200

    Moving sinple page components out of pages folder

--- src/routes/app.routes.ts
+++ src/routes/app.routes.ts
@@ -1,5 +1,5 @@
-import Dashboard from "../features/dashboard/pages/Dashboard";
-import Profile from "../features/profile/pages/Profile";
+import Dashboard from "../features/dashboard/Dashboard";
+import Profile from "../features/profile/Profile";
 import AppLayout from "../layouts/AppLayout";
 
 export const appRoutes = [
